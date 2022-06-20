750415bf1bedd97d8b5b0a2a67fdeaa57e2587f7
Jaime <jaime.perez@uc.cl>
2022-06-20 18:42:09 -0400

    Implementado boton para importar usuarios

---
+++ Sites/index.php
@@ -0,0 +1,21 @@
+<?php include("header.html") ?>
+
+<h2>Importar Usuarios</h2>
+<form method="post">
+	<input type="hidden" name="importar" value="1">
+	<input type="submit" value="Empezar">
+</form>
+
+
+<?php 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if ($_POST["importar"] === "1") {
+		// TODO importar	
+		echo "HOLA!!!";
+	}
+	// TODO: _POST login etc
+}
+
+?>
+
+<?php include("footer.html") ?>
