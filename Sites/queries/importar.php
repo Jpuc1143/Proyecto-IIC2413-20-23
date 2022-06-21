0fcd2cf11d06646c5d026fb8f90f75bfd57d7af3
Jaime <jaime.perez@uc.cl>
2022-06-20 22:44:45 -0400

    Implementado contraseñas aleatorias

--- Sites/queries/importar.php
+++ Sites/queries/importar.php
@@ -8,10 +8,13 @@ $db_impar -> exec("INSERT INTO Usuarios VALUES (default,'DGAC', 'admin', 'admin'
 // TODO contraseñas segun enunciado 
 $db_impar -> exec("
 	INSERT INTO usuarios (usuario, contrasena, tipo)
-	SELECT codigo, '1234', 'compania_aerea' FROM compania_aerea ON CONFLICT DO NOTHING;");
+	SELECT codigo, to_char(random()*100000000, 'fm00000000'),
+	'compania_aerea' FROM compania_aerea ON CONFLICT DO NOTHING;");
 $db_impar -> exec("
 	INSERT INTO usuarios (usuario, contrasena, tipo)
-	SELECT pasaporte, '1234', 'pasajero' FROM persona ON CONFLICT DO NOTHING;");
+	SELECT pasaporte, CONCAT(SUBSTRING(nombre, 1, 3),
+		SUBSTRING(pasaporte, 1, 3), to_char(random() * 100, 'fm00')
+	), 'pasajero' FROM persona ON CONFLICT DO NOTHING;");
 
 $db_impar -> commit();
 
