f994ad975ae3beb2a15174400b63a2cbf82154ca
Jaime <jaime.perez@uc.cl>
2022-06-25 19:23:51 -0400

    Limpiado estructura de proyecto

--- Sites/queries/usuarios_schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Usuarios;
-
-CREATE TABLE Usuarios(
-	id serial PRIMARY KEY,
-	usuario varchar UNIQUE NOT NULL,
-	contrasena varchar NOT NULL, -- En realidad debería ser un has de la contraseña, no la misma en plaintext.
-	tipo varchar NOT NULL CHECK(tipo = 'admin' OR tipo = 'compania_aerea' OR tipo = 'pasajero')
-)
