e9efe8a411a4517e632757887870058c99c49764
BFry712 <bfry@uc.cl>
2022-06-26 14:00:57 -0400

    compania aerea

--- Sites/compania_aerea.php
+++ Sites/compania_aerea.php
@@ -40,7 +40,7 @@ $result3 -> execute();
 $vuelos_rechazados = $result3 -> fetchAll(PDO::FETCH_NUM);
 ?>
     <h1>Vista de Compañia Aérea</h1>
-    <?php echo '<h2> Bienvenidoo $nombre_compania_aerea '.count($vuelos_aceptados).'</h2>'; ?>
+    <h2> Bienvenido</h2>  <?php echo "$nombre_compania_aerea" ?>
     <br>
     <hr>
     <h3> Su lista de vuelos aceptados es la siguiente</h3>
