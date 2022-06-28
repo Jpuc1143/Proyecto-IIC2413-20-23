e7e9b418441b32ee454e6f54cf19df784aee81c7
Jaime <jaime.perez@uc.cl>
2022-06-28 13:18:10 -0400

    Mensajes de error en la db par

--- Sites/config/databaseconnect.php
+++ Sites/config/databaseconnect.php
@@ -8,6 +8,9 @@
     $password2 = 'RinkuFry';
     $databaseName2 = 'grupo23e3';
     $db2 = new PDO("pgsql:dbname=$databaseName2;host=localhost;port=5432;user=$user2;password=$password2");
+
+    $db -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    $db2 -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
